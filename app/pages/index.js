cccd2246ffb877de2aa00c3ee81b25417aa69649
Roberto Díaz <robertocdiazsanchez@gmail.com>
2022-06-09 21:04:46 -0500

    project initialization
    + Added some of the basic required dependencies (hardhat, dotenv, @openzepellin's contracts).
    + Created basic app project structure using Next.js.

---
+++ app/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="app">
+      <Head>
+        <title>NFT Collection</title>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      </Head>
+    </div>
+  )
+}
