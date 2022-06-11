ec90a56198e1ada7bc5d6dbd7476bac44248c853
Roberto Díaz <robertocdiazsanchez@gmail.com>
2022-06-11 00:36:32 -0500

    finished frontend
    + Added NFTs resources as .svg files.
    * Finished frontend UI and the previous written functionality was also implemented.

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-import { Contracts, providers, utils } from require("ethers");
+import { Contract, providers, utils } from "ethers";
 import { NFT_CONTRACT_ABI, NFT_CONTRACT_ADDRESS } from "../constants/index";
-import { Signer } from 'ethers';
+import Web3Modal from 'web3modal';
 
 export default function Home() {
   // state variables
@@ -13,7 +13,7 @@ export default function Home() {
   const [hasPresaleEnded, setHasPresaleEnded] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [isOwner, setIsOwner] = useState(false);
-  const [mintedCount, setMintedCount] = useState(false);
+  const [mintedCount, setMintedCount] = useState(0);
 
   // modal used to connect to metamask
   const web3Modal = useRef();
@@ -26,9 +26,10 @@ export default function Home() {
    */
   const getProviderOrSigner = async (shouldBeSigner = false) => {
     const provider = await web3Modal.current.connect();
-    const web3Provider = await providers.Web3Provider(provider);
+    const web3Provider = new providers.Web3Provider(provider);
 
-    if (web3Provider.getNetwork() !== 4) {
+    
+    if ((await web3Provider.getNetwork()).chainId !== 4) {
       window.alert("Change to Rinkeby network");
       throw new Error("Change to Rinkeby network");
     }
@@ -42,75 +43,71 @@ export default function Home() {
    * @param {boolean} withSigner - Set to true if a signer will be needed to operate using on the contract.
    * @returns A contract instance to operate on the blockchain.
    */
-  const contractInstance = (withSigner = false) => {
+  const contractInstance = async (withSigner = false) => {
     return new Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, await getProviderOrSigner(withSigner));
-  }
-
-  /**
-   * Creates a new instance of the NFT Contract so any function contained in it can be called.
-   * 
-   * @param {(Contract) => any} contractFunction - Function that returns the (already awaited) function to be called usign the contract.
-   * @param {() => any} fallbackFunction - What to do after the transaction has been mined
-   * @param {boolean} needSigner - Set to true if a signer is needed to execute the function. Otherwise, it will be called using a provider.
-   */
-  const contractTransaction = async (contractFunction, fallbackFunction, needSigner = false) => {
-    try {
-      const contract = contractInstance(needSigner);
-
-      const tx = await contractFunction(contract);
-
-      setIsLoading(true);
-      await tx.wait();
-      setIsLoading(false);
-
-      fallbackFunction();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  
+  }  
 
   /**
    * Tries to mint one WNC NFT for the current user address.
    * 
-   * @param {*} isPresaleMint - Defines if the minting should be tried in the presale or not.
+   * @param {boolean} isPresaleMint - Defines if the minting should be tried in the presale or not.
    */
   const mintNFT = async (isPresaleMint = false) => {
-    contractTransaction({
-      contractFunction: async (contract) => {
-        // options to be passed on each mint operation
-        const mintOptions = {
-          // value signifies the cost of one WNC which, as the contract defines, is 0.001 ETH.
-          value: utils.parseEther("0.001"),
-        };
+    try {
+      const contract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        NFT_CONTRACT_ABI,
+        await getProviderOrSigner(true),
+      );
 
-        // creates transaction for mint operation
-        const tx = isPresaleMint ? 
-          contract.presaleMint(mintOptions) :
-          contract.mint(mintOptions);
+      // options to be passed on each mint operation
+      const mintOptions = {
+        // value signifies the cost of one WNC which, as the contract defines, is 0.001 ETH.
+        value: utils.parseEther("0.001"),
+      };
 
-        return tx;
-      },
-      fallbackFunction: () => {
-        alert("You've successfully minted a WNC NFT!");
-      },
-      needSigner: true,
-    });
+      // creates transaction for mint operation
+      const tx = isPresaleMint ? 
+        await contract.presaleMint(mintOptions) :
+        await contract.mint(mintOptions);
+
+      // wait for transaction to be mined
+      setIsLoading(true);
+      await tx.wait();
+      setIsLoading(false);
+      
+      alert("You've successfully minted a WNC NFT!");
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   /**
    * Tries to start the presale.
    */
   const startPresale = async () => {
-    await contractTransaction({
-      contractFunction: async (contract) => {
-        return contract.startPresale();
-      },
-      fallbackFunction: () => {
-        await checkIfPresaleStarted();
-      },
-      needSigner: true,
-    });
+    // await contractTransaction({
+    //   contractFunction: contract => contract.startPresale,
+    //   fallbackFunction: async () => {
+    //     await checkIfPresaleStarted();
+    //   },
+    //   needSigner: true,
+    // });
+
+    try {
+      const signer = await getProviderOrSigner(true);
+      const contract = new Contract(NFT_CONTRACT_ADDRESS, NFT_CONTRACT_ABI, signer);
+
+      const tx = await contract.startPresale();
+
+      setIsLoading(true);
+      await tx.wait();
+      setIsLoading(false);
+
+      await checkIfPresaleStarted();
+    } catch(err) {
+      console.error(err)
+    };
   }
 
   /**
@@ -120,7 +117,7 @@ export default function Home() {
    */
   const checkIfPresaleStarted = async () => {
     try {
-      const contract = contractInstance();
+      const contract = await contractInstance();
 
       const hasStartedYet = await contract.presaleStarted();
 
@@ -143,9 +140,11 @@ export default function Home() {
    */
   const checkIfPresaleHasEnded = async () => {
     try {
-      const contract = contractInstance();
+      const contract = await contractInstance();
 
-      const hasEndedYet = await contract.presaleEnded();
+      const endStamp = await contract.presaleEnded();
+
+      const hasEndedYet = endStamp.lt(Math.floor(Date.now() / 1000));
 
       setHasPresaleEnded(hasEndedYet);
 
@@ -160,13 +159,13 @@ export default function Home() {
    */
   const checkIfOwner = async () => {
     try {
-      const contract = contractInstance();
+      const contract = await contractInstance();
 
       // gets the actual address of the owner of the contract
       const owner = await contract.owner();
 
       // gets user's address
-      const address = (await getProviderOrSigner(true)).getAddress();
+      const address = await (await getProviderOrSigner(true)).getAddress();
 
       // only sets isOwner state variable to true if it is in fact the owner
       // otherwise, it will not touch state, preventing rerendering
@@ -182,7 +181,7 @@ export default function Home() {
    */
   const updateTokensCount = async () => {
     try {
-      const contract = contractInstance();
+      const contract = await contractInstance();
 
       setMintedCount(await contract.tokenIds());
     } catch(err) {
@@ -203,12 +202,103 @@ export default function Home() {
     }
   }
 
+  useEffect(() => {
+    if (!isWalletConnected) {
+      // if wallet is not connected, create a new instance of a web3modal
+      web3Modal.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+
+      // then connect to wallet
+      connectToWallet();
+
+      // check every 5 seconds if presale has started/ended
+      // when ended, this interval is cleared
+      const presaleEndedInterval = setInterval(async () => {
+        const presaleStarted = await checkIfPresaleStarted();
+        if (presaleStarted) {
+          const presaleEnded = await checkIfPresaleHasEnded();
+
+          if (presaleEnded) {
+            clearInterval(presaleEndedInterval);
+          }
+        }
+      }, 5 * 1000);
+
+      // when connecting to wallet for the first time, initiate an
+      // interval that will update the count of minted nfts every 5 seconds
+      setInterval(async () => {
+        await updateTokensCount();
+      }, 5 * 1000);
+    }
+  }, [isWalletConnected]);
+
+  const StateButton = () => {
+    // if currently loading, show a `loading` message
+    if (isLoading) {
+      return (
+        <div className={styles.msgButton}>
+          Loading...
+        </div>
+      );
+    }
+
+    // if wallet not connected, return a button to connect to wallet
+    if (!isWalletConnected) {
+      return (<div onClick={connectToWallet} className={styles.button}>
+        Connect to wallet
+      </div>);
+    }
+
+    // if presale has not started and the user is the owner, allow them to start presale
+    if (!hasPresaleStarted && isOwner) {
+      return (<div onClick={startPresale} className={styles.button}>
+        Start presale
+      </div>);
+    }
+
+    // if presale has not started yet, show a `presale has not started yet` message
+    if (!hasPresaleStarted) {
+      return <div className={styles.msgButton}>
+        Presale has not started yet
+      </div>
+    }
+
+    // at this point, the presale has started, so check if presale has ended or not
+    // if not, enable a mint button that says to mint only if whitelisted
+    if (!hasPresaleEnded) {
+      return <div onClick={async () => await mintNFT(true)} className={styles.button}>
+        Presale Mint! *
+      </div>
+    }
+
+    // if we reach here, then presale has already ended, so enable a public mint button
+    return <div onClick={async () => await mintNFT()} className={styles.button}>
+      Public mint!
+    </div>
+  }
+
   return (
-    <div className="app">
+    <div className={styles.app}>
       <Head>
         <title>NFT Collection</title>
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
+      <div className={styles.main}>
+        <div className={styles.info}>
+          <h1>Welcome to the WNC NFTs website!</h1>
+          <div className={styles.description}>
+            It's an NFT collection for crypto devs.
+          </div>
+          <div className={styles.description}>
+            {mintedCount}/20 have been minted
+          </div>
+          { StateButton() }
+        </div>
+        <img className={styles.image} src="./nfts/1.svg" alt="NFTs logo" />
+      </div>
     </div>
   )
 }
